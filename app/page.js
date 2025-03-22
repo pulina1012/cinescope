9c4bb05f5c1fdd97362f74f2754d30c41d732b55
pulina1012 <pulinamahima26@gmail.com>
2025-03-22 20:04:45 +0530

    session 6 - initial commit

--- app/page.js
+++ app/page.js
@@ -1,9 +1,26 @@
 import Image from "next/image";
+import MyFirstComponent from "../components/MyFirstComponent";
+
+// const Home2 = () =>
+// {
+//   return (
+//     <div>
+//     <MyFirstComponent />
+//   </div>
+//   );
+// };
+
+// export default Home2;
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <MyFirstComponent />
+        <hr
+          style={{ color:" #000" , backgroundColor: "white", height: "5px" }}
+        />
+
         <Image
           className="dark:invert"
           src="/next.svg"

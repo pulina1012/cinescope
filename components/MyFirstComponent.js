e881c596bd1a64690381c1262c4b4f016792d425
pulina1012 <pulinamahima26@gmail.com>
2025-03-29 19:14:01 +0530

    week 6 practical

---
+++ components/MyFirstComponent.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const MyFirstComponent = () => {
+  return (
+    <div>
+
+      <h1>hello World!</h1>
+      <p>This is my first Next.js component.</p>
+    </div>
+
+  );
+};
+
+export default MyFirstComponent;
